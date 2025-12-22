1ef515e091bafd98acc7ca625c56a0026e0b38a0
shadow-eljan <eljanrai25@gmail.com>
2025-12-22 20:18:11 +0545

    Model section Notes

---
+++ ExpressClass/Models/Notes.js
@@ -0,0 +1,9 @@
+const notes = [
+    {
+        id: 1,
+        title: "Totle 1",
+        content: "Content 1"
+    },
+    {id: 2, title: "Title 2", content: "Content 2"},
+]
+export default notes;
