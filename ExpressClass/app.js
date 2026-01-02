dca6ed0203223ea22ab00e1f530b1365c4ca76f4
shadow-eljan <eljanrai25@gmail.com>
2026-01-02 20:14:35 +0545

    Small fixes

--- ExpressClass/app.js
+++ ExpressClass/app.js
@@ -2,8 +2,14 @@ import express from "express";
 import { getNotes , addNotes, updateNotes, deleteNotes} from "./Controller/NoteController.js";
 import router from "./Routers/noteRoutes.js";
 import logger from "./Logging/Logger.js";
+import mongoose from "mongoose"
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/notesdb")
+.then((conn) => console.log(`Connection successfull at ${conn.connection.host}`))
+.catch((err) => console.log("Error connecting to db.", err.message))
+
 app.use(express.json());
 
 app.use(logger);
